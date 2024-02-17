a4909b7ba8be12b1cc4a9839fffd7e8ae27cdd82
k0mronbek <153188218+k0mronbek@users.noreply.github.com>
2024-02-17 15:38:07 +0500

    Create map.c

---
+++ c/map.c
@@ -0,0 +1,75 @@
+#include "../h/map.h"
+#include "../h/my_string.h"
+
+int three_in_min(int** map, short index1, short index2) {
+    short min = -1, i1, i2, i3;
+    i1 = map[index1 - 1][index2 -1];
+    i2 = map[index1 - 1][index2];
+    i3 = map[index1][index2 -1];
+    if (i1 > 0 && (min == -1 || i1 < min)) {
+        min = i1;
+    }
+    if (i2 > 0 && (min == -1 || i2 < min)) {
+        min = i2;
+    }
+    if (i3 > 0 && (min == -1 || i3 < min)) {
+        min = i3;
+    }
+    return min;
+}
+
+int* max_index(int** map) {
+    short max = -1,i, j;
+    int* max_ind = (int*)malloc(sizeof(int)*2);
+    for(i = 0; map[i]; i++) {
+        for(j = 0; map[i][j] != -1; j++) {
+            if(map[i][j] > max) {
+                max = map[i][j];
+                max_ind[0] = i;
+                max_ind[1] = j;
+            }
+        } 
+    }
+    return max_ind;
+}
+
+int** map_designation(int** map, short ind1, short ind2) {
+    short index = 0, i, j;
+    for(i = 1; i <= ind2; i++) {
+        if(map[ind1 - i][ind2 - i] > 0 && map[ind1][ind2 - i] > 0 && map[ind1 - i][ind2] > 0) {
+            index++; 
+        }
+    }
+    map[ind1][ind2] = -2;
+    for(i = 1; i < index; i++) {
+        map[ind1 - i][ind2] = -2;
+        map[ind1][ind2 - i] = -2;
+        for(j = 1; j < index; j++) {
+            map[ind1 - i][ind2 - j] = -2;
+        }
+    }
+    for(i = 0; map[i]; i++) {
+        for(j = 0; map[i][j] != -1; j++) {
+            if(map[i][j] > 0) {
+                map[i][j] = 1;
+            }
+        }
+    }
+    return map; 
+}
+
+int** my_square(int** map) {
+    short index1 = 1, index2 = 1;
+    short flag;
+    for( ; map[index1]; index1++) {
+        for(index2 = 1 ; map[index1][index2] != -1; index2++) {
+            if(map[index1][index2] > 0) {
+                map[index1][index2] += three_in_min(map, index1, index2);
+            }
+        }
+    }
+    int* max_ind = max_index(map);
+    map = map_designation(map, max_ind[0], max_ind[1]);
+    free(max_ind);
+    return map;
+}
