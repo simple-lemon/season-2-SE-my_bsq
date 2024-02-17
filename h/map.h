e04ae2481ee7a411e310c0b1108d84eef8e98138
jasurbohodirov <132946381+jasurbohodirov@users.noreply.github.com>
2024-02-17 15:13:31 +0500

    Create map.h

---
+++ h/map.h
@@ -0,0 +1,7 @@
+#ifndef MY_BSQ_H_MAP_H_
+#define MY_BSQ_H_MAP_H_
+
+int** my_square(int** map);
+int three_in_min(int** map, short index1, short index2);
+int** map_designation(int** map, short ind1, short ind2);
+#endif
