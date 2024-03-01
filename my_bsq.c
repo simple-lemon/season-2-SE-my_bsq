2d7b36601f4f7300f96c17a3f9e4e8acd3d2c20c
k0mronbek <153188218+k0mronbek@users.noreply.github.com>
2024-03-01 12:28:50 +0500

    Update my_bsq.c

--- my_bsq.c
+++ my_bsq.c
@@ -1,43 +1,53 @@
-// gcc -g3 -fsanitize=address -o my_bsq my_bsq* c/my_string.c c/map.c
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
 #include "./h/my_string.h"
 #include "./h/map.h"
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+short error_check(char* str) {
+    short flag = 0;
+    
+    for(short i = 0; str[i]; i++) {
+        if(str[i] != '.' && str[i] != '\n' && str[i] != 'o') {
+            flag = 1;
+        }
+        if(flag == 1) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
-int error(short flag) {
-    if(flag == 1) printf("error map\n");
+short error(short flag) {
+    if(flag == 0) printf("error map\n");
+    if(flag == 1) printf("No such file\n");
     return 1;
 }
 
 int main(int ac, char** av) {
-    if(ac == 1) {
-        return error(1);
-    }
+    if(ac == 1) return error(1);
     short fd = open(av[1], O_RDONLY);
-    if(fd == -1) {
-        return error(1);
-    }
+    if(fd == -1) return error(1);
     struct stat f_stat;
     stat(av[1], &f_stat);
     short size = (short)f_stat.st_size;
-    char *content = (char*)calloc(sizeof(char), size + 1);
-    short flag = read(fd, content, size);
-    if(flag == 0) {
+    if(size == 0) return error(0);
+    char* content = (char*)calloc(sizeof(char), size + 1);
+    read(fd, content, size);
+    if(error_check(content) == 1) {
         free(content);
-        return error(1);
+        return error(0);
     }
-    int** map = my_split(content);
-    map = my_square(map);
-    free(content);
     close(fd);
+    int** map = my_split(content);
+    free(content);
+    map = my_square(map);
     char** arr = redraw_map(map);
-    for(flag = 0; arr[flag]; flag++) {printf("%s\n", arr[flag]);}
-    for(flag = 0; map[flag]; flag++) {free(map[flag]);}
-    for(flag = 0; arr[flag]; flag++) {free(arr[flag]);}
-    free(arr);
+    for(fd = 0; arr[fd]; fd++) {printf("%s\n", arr[fd]);}
+    for(fd = 0;arr[fd];fd++) {free(arr[fd]);}
+    for(fd = 0; map[fd]; fd++) {free(map[fd]);}
     free(map);
+    free(arr);
     return 0;
 }
