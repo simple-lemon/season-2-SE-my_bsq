0896f32279767fca63189a3fb2b749848cb2efff
k0mronbek <153188218+k0mronbek@users.noreply.github.com>
2024-03-01 12:27:08 +0500

    Update my_string.c

--- c/my_string.c
+++ c/my_string.c
@@ -1,73 +1,68 @@
 #include "../h/my_string.h"
 
-void fill_minus(int** map, short index1, short index2) {
+void fill_minus(int** map, short ind1, short ind2) {
     short i, j;
-    for(i = 0; i<index1; i++) {
-        for(j = 0; j < index2; j++) {
+    for(i = 0; i<ind1; i++) {
+        for(j = 0; j<ind2; j++) {
             map[i][j] = -1;
         }
     }
 }
 
 int** my_split(char* content) {
-    short size1 = 1,size2 = 0;
-    short i;
+    short size1 = 3, size2 = 2, i;
     for(i = 0; content[i]; i++) {
         if(content[i] == '\n') size1++;
     }
-    int** map = (int**)malloc(sizeof(int**)* (size1 + 1));
+    int** map = (int**)malloc(sizeof(int*)*(size1 + 1));
     map[size1] = NULL;
-    for(;content[size2] != '\n'; size2++) {}
-    for(i=0; i<size1; i++) {
-        map[i] = (int*)malloc(sizeof(int*)*(size2 + 1));
+    for(i = 0;content[i] != '\n'; i++) {size2++;}
+    for(i = 0; i<size1;i++) {
+        map[i] = (int*)malloc(sizeof(int*)*size2);
     }
-    fill_minus(map, size1, size2 + 1);
-    size1 = 0;
-    size2 = 0;
-    for(i=0; content[i]; i++) {
+    fill_minus(map, size1, size2);
+    size1 = 1;
+    size2 = 1;
+    for(i = 0;content[i]; i++) {
         if(content[i] == '\n') {
             size1++;
-
-            size2 = 0;
-            continue;
+            size2 = 1;
         }
         else if(content[i] == '.') {
             map[size1][size2++] = 1;
         }
-        else if(content[i] == 'o'){
+        else if(content[i] == 111) {
             map[size1][size2++] = 0;
         }
     }
     return map;
 }
 
+
 char** redraw_map(int** map) {
-    short len1 = 0, len2 = 0, i, j;
-    for(; map[len1]; len1++) {
-        for(len2 = 0;map[len1][len2] != -1; len2++);
+    short len1 = 1, len2 = 1, i, j, ind1, ind2;
+    for(; map[len1][1] != -1; len1++) {
+        for(len2 = 1;map[len1][len2] != -1; len2++);
     }
-    char** arr = (char**)malloc(sizeof(char*)*(len1 + 1));
-    arr[len1] = NULL;
-    for(i = 0; i < len1; i++) {
-        arr[i] = (char*)calloc(sizeof(char), len2+1);
-        for(j = 0; j < len2; j++) {
-            if(map[i][j] == 1) {
+    char** arr = (char**)malloc(sizeof(char*)*(len1));
+    arr[len1 -1] = NULL;
+    ind1 = 1;
+    for(i = 0; i < len1 - 1; i++) {
+        arr[i] = (char*)calloc(sizeof(char), len2);
+        ind2 = 1;;
+        for(j = 0; j < len2 - 1; j++) {
+            if(map[ind1][ind2] > 0) {
                 arr[i][j] = '.';
             }
-            if(map[i][j] == 0) {
+            if(map[ind1][ind2] == 0) {
                 arr[i][j] = 'o';
             }
-            if(map[i][j] == -2) {
+            if(map[ind1][ind2] == -2) {
                 arr[i][j] = 'x';
             }
+            ind2++;
         }
+        ind1++;
     }
     return arr;
-}
-
-short my_isdigit(char c) {
-    if(c >= 48 && c <= 57) {
-        return 1;
-    }
-    return 0;
 }
