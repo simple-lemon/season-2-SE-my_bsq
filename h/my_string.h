883c7b11047575b4a4ffb99d566cb6dd6e7dbfae
k0mronbek <153188218+k0mronbek@users.noreply.github.com>
2024-03-01 12:27:56 +0500

    Update my_string.h

--- h/my_string.h
+++ h/my_string.h
@@ -1,9 +1,7 @@
 #ifndef MY_BSQ_H_MY_STRING_H_
 #define MY_BSQ_H_MY_STRING_H_
-
 #include <stdlib.h>
 int** my_split(char* content);
-short my_isdigit(char c);
-void fill_minus(int** map, short index1, short index2);
 char** redraw_map(int** map);
+void fill_minus(int** map, short ind1, short ind2);
 #endif
