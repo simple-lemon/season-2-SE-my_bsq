3c214ba92454df1eaad774788ac2f40427a856f3
Nurulloh <140059040+abdurashidovnurulloh@users.noreply.github.com>
2024-03-02 07:55:36 +0500

    Update my_string.c

--- c/my_string.c
+++ c/my_string.c
@@ -1,43 +1,29 @@
-#include "../h/map.h"
 #include "../h/my_string.h"
 
-char** square(char** map) {
-    short i, j, ind, len, flag, index;
-    int* indexes = (int*)malloc(sizeof(int)*3);
-    indexes[0] = 0;
-    indexes[1] = 0;
-    indexes[2] = 0;
-    for(i = 2; map[i]; i++) {
-        for(j = 2; map[i][j]; j++) {
-            len = 0;
-            for(ind = 0; map[i-ind][j] && map[i][j-ind]; ind++) {
-                if(map[i][j - ind] != 'o' && map[i - ind][j] != 'o') {
-                    flag = 0;
-                    for(index = 0; map[i - ind][j -index]; index++) {
-                        if(map[i-ind][j-index] == 'o') {
-                            flag = 1;
-                            break;
-                        }
-                    }
-                }
-                if(flag == 0) {
-                        len++;
-                    }
-            }
-            if(indexes[2] < len) {
-                indexes[0] = i;
-                indexes[1] = j;
-                indexes[2] = len;
-            }
+void fill_null(char* str, short over) {
+    for(short i = 0; i < over; i++) {
+        str[i] = '\0';
+    }
+}
+
+char** my_split(char* content) {
+    short i = 0, j = 1, ind = 1, size = 3;
+    for( ; content[i]; i++) {
+        if(content[i] == '\n') size++;
+    }
+    char** arr = (char**)malloc(sizeof(char*)*(size + 1));
+    arr[size] = NULL;
+    for(size = 0; content[size] != '\n'; size++);
+    for(i = 0; arr[i]; i++) {
+        arr[i] = (char*)calloc(sizeof(char), size+5);
+    }
+    for(i = 0; content[i]; i++) {
+        if(content[i] != '\n') {
+            arr[ind][j++] = content[i];
+        } else {
+            ind++;
+            j = 1;
         }
     }
-    for(ind = 0; ind < indexes[2]-1; ind++) {
-        map[indexes[0] - ind][indexes[1]] = 'x';
-        map[indexes[0]][indexes[1]-ind] = 'x';
-        for(index = 0; index < indexes[2]-1; index++) {
-            map[indexes[0]-ind][indexes[1]-index] = 'x';
-        }
-    }
-    free(indexes); 
-    return map;
+    return arr;
 }
