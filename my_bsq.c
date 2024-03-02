7bc76d9d6d24be2f0aaf52791f81727dce438141
Nurulloh <140059040+abdurashidovnurulloh@users.noreply.github.com>
2024-03-02 07:58:05 +0500

    Update my_bsq.c

--- my_bsq.c
+++ my_bsq.c
@@ -1,43 +1,67 @@
-// gcc -g3 -fsanitize=address -o my_bsq my_bsq* c/my_string.c c/map.c
 #include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include "./h/my_string.h"
 #include "./h/map.h"
 
+short content_check(char* str) {
+    short line1 = 0, line2 = 0, i, flag;
+    for(i = 0; str[i]; i++) {
+        flag = 0;
+        if(str[i] != '.' && str[i] != 'o' && str[i] != '\n') {
+            flag = 1;
+        }
+        if(flag == 1) {
+            return 1;
+        }
+    }
+    for(i = 0; str[i]; i++) {
+        line1++;
+        if(str[i] == '\n') {
+            if(line2 != 0 && line1 != 0 && line1 != line2) {
+                return 1;
+            }
+            line2 = line1;
+            line1 = 0;
+        }
+    }
+    return 0;
+}
 
-int error(short flag) {
-    if(flag == 1) printf("error map\n");
+short error(short flag) {
+    if(flag == 0) printf("no such file\n");
+    if(flag == 1) printf("wrong map\n");
     return 1;
 }
 
 int main(int ac, char** av) {
-    if(ac == 1) {
-        return error(1);
-    }
+    if(ac == 1) return error(0);
     short fd = open(av[1], O_RDONLY);
-    if(fd == -1) {
+    if(fd == -1) return error(0);
+    struct stat file_stat;
+    stat(av[1], &file_stat);
+    short size = (short)file_stat.st_size;
+    if(size == 0) {
+        close(fd);
         return error(1);
     }
-    struct stat f_stat;
-    stat(av[1], &f_stat);
-    short size = (short)f_stat.st_size;
-    char *content = (char*)calloc(sizeof(char), size + 1);
-    short flag = read(fd, content, size);
-    if(flag == 0) {
+    char* content = (char*)calloc(sizeof(char), size + 1);
+    read(fd, content, size);
+    if(content_check(content) == 1) {
         free(content);
         return error(1);
     }
-    int** map = my_split(content);
-    map = my_square(map);
+    char** arr = my_split(content);
+    arr = square(arr);
+    for(size = 1; arr[size]; size++) {
+        if(arr[size][1]) {
+            printf("%s\n", &arr[size][1]);
+        }
+    }
+    for(size = 0; arr[size]; size++) {free(arr[size]);}
+    free(arr);
     free(content);
     close(fd);
-    char** arr = redraw_map(map);
-    for(flag = 0; arr[flag]; flag++) {printf("%s\n", arr[flag]);}
-    for(flag = 0; map[flag]; flag++) {free(map[flag]);}
-    for(flag = 0; arr[flag]; flag++) {free(arr[flag]);}
-    free(arr);
-    free(map);
     return 0;
 }
