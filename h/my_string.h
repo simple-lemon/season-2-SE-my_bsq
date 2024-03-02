87b864b141fdcf8331da2d46b12ed04a64439d4a
Khalid <133192175+Kenjayev2002A@users.noreply.github.com>
2024-03-02 11:53:34 +0500

    Create my_string.h

---
+++ h/my_string.h
@@ -0,0 +1,7 @@
+#ifndef MY_BSQ_H_MY_STRING_H_
+#define MY_BSQ_H_MY_STRING_H_
+#include <stdlib.h>
+int** my_split(char* content);
+char** redraw_map(int** map);
+void fill_minus(int** map, short ind1, short ind2);
+#endif
