ef43947f1e20c2c66e7d6e298d01a6d53e5d8a3b
Nurulloh <140059040+abdurashidovnurulloh@users.noreply.github.com>
2024-03-02 07:55:58 +0500

    Update map.c

--- c/map.c
+++ c/map.c
@@ -1,75 +1,43 @@
 #include "../h/map.h"
 #include "../h/my_string.h"
 
-int three_in_min(int** map, short index1, short index2) {
-    short min = -1, i1, i2, i3;
-    i1 = map[index1 - 1][index2 -1];
-    i2 = map[index1 - 1][index2];
-    i3 = map[index1][index2 -1];
-    if (i1 > 0 && (min == -1 || i1 < min)) {
-        min = i1;
-    }
-    if (i2 > 0 && (min == -1 || i2 < min)) {
-        min = i2;
-    }
-    if (i3 > 0 && (min == -1 || i3 < min)) {
-        min = i3;
-    }
-    return min;
-}
-
-int* max_index(int** map) {
-    short max = -1,i, j;
-    int* max_ind = (int*)malloc(sizeof(int)*2);
-    for(i = 0; map[i]; i++) {
-        for(j = 0; map[i][j] != -1; j++) {
-            if(map[i][j] > max) {
-                max = map[i][j];
-                max_ind[0] = i;
-                max_ind[1] = j;
+char** square(char** map) {
+    short i, j, ind, len, flag, index;
+    int* indexes = (int*)malloc(sizeof(int)*3);
+    indexes[0] = 0;
+    indexes[1] = 0;
+    indexes[2] = 0;
+    for(i = 2; map[i]; i++) {
+        for(j = 2; map[i][j]; j++) {
+            len = 0;
+            for(ind = 0; map[i-ind][j] && map[i][j-ind]; ind++) {
+                if(map[i][j - ind] != 'o' && map[i - ind][j] != 'o') {
+                    flag = 0;
+                    for(index = 0; map[i - ind][j -index]; index++) {
+                        if(map[i-ind][j-index] == 'o') {
+                            flag = 1;
+                            break;
+                        }
+                    }
+                }
+                if(flag == 0) {
+                        len++;
+                    }
             }
-        } 
-    }
-    return max_ind;
-}
-
-int** map_designation(int** map, short ind1, short ind2) {
-    short index = 0, i, j;
-    for(i = 1; i <= ind2; i++) {
-        if(map[ind1 - i][ind2 - i] > 0 && map[ind1][ind2 - i] > 0 && map[ind1 - i][ind2] > 0) {
-            index++; 
-        }
-    }
-    map[ind1][ind2] = -2;
-    for(i = 1; i < index; i++) {
-        map[ind1 - i][ind2] = -2;
-        map[ind1][ind2 - i] = -2;
-        for(j = 1; j < index; j++) {
-            map[ind1 - i][ind2 - j] = -2;
-        }
-    }
-    for(i = 0; map[i]; i++) {
-        for(j = 0; map[i][j] != -1; j++) {
-            if(map[i][j] > 0) {
-                map[i][j] = 1;
+            if(indexes[2] < len) {
+                indexes[0] = i;
+                indexes[1] = j;
+                indexes[2] = len;
             }
         }
     }
-    return map; 
-}
-
-int** my_square(int** map) {
-    short index1 = 1, index2 = 1;
-    short flag;
-    for( ; map[index1]; index1++) {
-        for(index2 = 1 ; map[index1][index2] != -1; index2++) {
-            if(map[index1][index2] > 0) {
-                map[index1][index2] += three_in_min(map, index1, index2);
-            }
+    for(ind = 0; ind < indexes[2]-1; ind++) {
+        map[indexes[0] - ind][indexes[1]] = 'x';
+        map[indexes[0]][indexes[1]-ind] = 'x';
+        for(index = 0; index < indexes[2]-1; index++) {
+            map[indexes[0]-ind][indexes[1]-index] = 'x';
         }
     }
-    int* max_ind = max_index(map);
-    map = map_designation(map, max_ind[0], max_ind[1]);
-    free(max_ind);
+    free(indexes); 
     return map;
 }
